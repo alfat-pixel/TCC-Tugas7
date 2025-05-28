32cbec64ae18f647a444441291d3f23ca7338df8
alfatrahman <162115441+alfatrahman@users.noreply.github.com>
2025-05-29 00:01:36 +0700

    Update baru tugas 7

--- backend/model/notesModel.js
+++ backend/model/notesModel.js
@@ -3,7 +3,7 @@ import db from "../config/database.js";
 
 const {DataTypes} = Sequelize
 
-const Notes = db.define("notesalfat",{
+const Notes = db.define("notes",{
     
     title: {
         type: DataTypes.STRING,
